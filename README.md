869f9f2d5e7b6a8943456e49eec8669e5ccfadcc
Dennis Crielaard <denniscrielaard@nl.abnamro.com>
2022-11-20 21:59:40 +0100

    changes

--- README.md
+++ README.md
@@ -13,5 +13,15 @@ curl --request POST \
   --data '{"name": "Dennis","price": 5.0}'
 ```
 
+Documentation of the code can be found at:
+http://127.0.0.1:8000/docs
+
+### Scalability
+Calls made to the API are syncronous. It may be helpful to send the requests a-syncronuous using asyncio.
+This can be tested using boom
+
+`boom http://127.0.0.1.8000/sleep_fast -c 20 -n 100`
+(100 requests send in batches of 20)
+
 This repo is based on the videos by calmcode.io (https://calmcode.io/fastapi/hello-world.html)
 
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import time
+import asyncio
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 app = FastAPI()
 
@@ -7,14 +9,34 @@ class Item(BaseModel):
     name: str
     price: float
 
+    @validator("price")
+    def price_must_be_positive(cls, value):
+        if value <= 0:
+            raise ValueError(f"We except price >0, we received {value}")
+        return value
+
 @app.get("/")
 async def root():
     return {"message": "hello world"}
 
 @app.get("/users/{user_id}")
 def read_user(user_id: int):
+    """
+    We accept a `user_id` here, and return a json-blob containing it.
+    """
     return {"user_id": user_id}
 
 @app.post("/items/")
 def create_item(item: Item):
     return item
+
+@app.get("/sleep_slow")
+def sleep_slow():
+    r = time.sleep(1)
+    return {"status": "done"}
+
+@app.get("/sleep_fast")
+async def sleep_fast():
+    r = await asyncio.sleep(1)
+    return {"status": "done"}
+
